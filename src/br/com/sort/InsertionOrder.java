f2083513e06e2e3a3c29999153609c5566e06966
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2023-05-21 19:43:16 -0300

    refactor: refatoração para a ordenação usando lista ficar mais parecida com a ordenação usando array

--- src/br/com/sort/InsertionOrder.java
+++ src/br/com/sort/InsertionOrder.java
@@ -22,18 +22,17 @@ public class InsertionOrder {
 	}
 
 	public static List<Long> order(List<Long> list) {
-		int size = list.size();
+		long key;
+		int i;
 
-		for (int i = 1; i < size; i++) {
-			Long key = list.get(i);
-			int j = i - 1;
+		for (int j = 1; j < list.size(); j++) {
+			key = list.get(j);
 
-			while (j >= 0 && list.get(j) > key) {
-				list.set(j + 1, list.get(j));
-				j--;
+			for (i = j - 1; (i >= 0) && (list.get(i) > key); i--) {
+				list.set(i + 1, list.get(i));
 			}
 
-			list.set(j + 1, key);
+			list.set(i + 1, key);
 		}
 
 		return list;
